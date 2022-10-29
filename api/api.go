4097ccc89a61b9dff58d12b669279cb64d7f9bc0
gross2001 <eldarradle29@gmail.com>
2022-10-29 10:49:35 +0300

    Change a error handling

--- api/api.go
+++ api/api.go
@@ -2,49 +2,72 @@ package api
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	"log"
 	"net/http"
+	"os"
 
 	"github.com/gross2001/weather/model"
 )
 
-var apiKey = "b7696781ba8d325ebc82c5f609f05210"
-var limit = "5"
+var (
+	limit  = "5"
+	domain = "http://api.openweathermap.org"
+)
 
-func GetCoordinates(cityName string) model.Cities {
+func getToken() (string, error) {
 
-	URL := "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=" + limit + "&appid=" + apiKey
+	if val, ok := os.LookupEnv("WEATHER_TOKEN"); ok {
+		return val, nil
+	}
+	err := errors.New("You should provide a WEATHER_TOKEN in enviromental variables")
+	return "", err
+}
+
+func GetCoordinates(cityName string) (model.Cities, error) {
+
+	var cities model.Cities
+
+	apiKey, err := getToken()
+	if err != nil {
+		return cities, err
+	}
+
+	URL := domain + "/geo/1.0/direct?q=" + cityName + "&limit=" + limit + "&appid=" + apiKey
 
 	resp, err := http.Get(URL)
 	if err != nil {
-		log.Fatal(err.Error())
+		return cities, err
 	}
 	defer resp.Body.Close()
 
-	var cities model.Cities
 	if err := json.NewDecoder(resp.Body).Decode(&cities); err != nil {
-		log.Fatal(err.Error())
+		return cities, err
 	}
-	fmt.Println(cities)
-	return cities
+	return cities, nil
 }
 
-func GetWeather(city model.City) model.WeatherRs {
+func GetWeather(city model.City) (model.WeatherRs, error) {
+
+	var weather model.WeatherRs
+
 	lat := fmt.Sprintf("%g", city.Lat)
 	lon := fmt.Sprintf("%g", city.Lon)
 
-	URL := "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
+	apiKey, err := getToken()
+	if err != nil {
+		return weather, err
+	}
 
+	URL := domain + "/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
 	resp, err := http.Get(URL)
 	if err != nil {
-		log.Fatal(err.Error())
+		return weather, err
 	}
 	defer resp.Body.Close()
 
-	var weather model.WeatherRs
 	if err := json.NewDecoder(resp.Body).Decode(&weather); err != nil {
-		log.Fatal(err.Error())
+		return weather, err
 	}
-	return weather
+	return weather, nil
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 
 	"github.com/gross2001/weather/api"
 )
@@ -12,9 +13,16 @@ func main() {
 	cityName := flag.String("city", "Berlin", "put the name of the city, for example -city London")
 	flag.Parse()
 
-	cities := api.GetCoordinates(*cityName)
+	cities, err := api.GetCoordinates(*cityName)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
 	for _, city := range cities {
-		weather := api.GetWeather(city)
+		weather, err := api.GetWeather(city)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
 		fmt.Println("for city with lon", weather.Coord.Lon, "and lat", weather.Coord.Lat, "temp", weather.Main.Temp)
 	}
 }
